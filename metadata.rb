c9799bf8ec4c217e7a5e93422a4bc53b8de006b3
Kirill Kouznetsov <agon.smith@gmail.com>
2018-01-15 17:55:44 +0800

    Release: 0.1.13
    * README.md update
    * CONTRIBUTION.md & TESTING.md were added.

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'agon.smith@gmail.com'
 license 'Apache-2.0'
 description 'Configure the system timezone on Debian or Ubuntu.'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '0.1.12'
+version '0.1.13'
 
 chef_version '>= 12.14', '< 14.0'
 
